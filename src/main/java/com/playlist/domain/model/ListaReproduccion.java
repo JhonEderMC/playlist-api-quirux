815f61b8315efc79226127f43ea3a8d5d60e4232
jhonederMC <jhoneder@hotmail.com>
2025-05-24 10:05:39 -0500

    Se añaden constructores y métodos para gestionar la lista de canciones en la clase ListaReproduccion

--- src/main/java/com/playlist/domain/model/ListaReproduccion.java
+++ src/main/java/com/playlist/domain/model/ListaReproduccion.java
@@ -4,15 +4,43 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
-@Builder
 public class ListaReproduccion {
 
     private String nombre;
     private String descripcion;
     private List<Cancion> canciones;
+
+    public ListaReproduccion() {
+        this.canciones = new ArrayList<>();
+    }
+
+    public ListaReproduccion(String nombre, String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.canciones = new ArrayList<>();
+    }
+
+    public ListaReproduccion(String nombre, String descripcion, List<Cancion> canciones) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.canciones = canciones != null ? canciones : new ArrayList<>();
+    }
+
+
+    public void setCanciones(List<Cancion> canciones) {
+        this.canciones = canciones != null ? canciones : new ArrayList<>();
+    }
+
+    public void addCancion(Cancion cancion) {
+        if (this.canciones == null) {
+            this.canciones = new ArrayList<>();
+        }
+        this.canciones.add(cancion);
+    }
 
 }
