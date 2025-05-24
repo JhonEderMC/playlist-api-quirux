c09b78ce1f998a30a5f284813de26ebd94c0a385
jhonederMC <jhoneder@hotmail.com>
2025-05-24 11:37:42 -0500

    Se crea el servicio CustomUserDetailsService para gestionar la carga de detalles del usuario en el contexto de seguridad

---
+++ src/main/java/com/playlist/infrastructure/entrypoints/web/scurity/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.playlist.infrastructure.entrypoints.web.scurity;
+
+import com.playlist.domain.model.Usuario;
+import com.playlist.domain.service.UsuarioService;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsuarioService usuarioService;
+
+    public CustomUserDetailsService(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = usuarioService.obtenerPorNombre(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado: " + username));
+
+        return new User(
+                usuario.getUsername(),
+                usuario.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + usuario.getRole()))
+        );
+    }
+}
