4d73ec0973ddfd99f2a482d4ab0ecacb03f31697
jhonederMC <jhoneder@hotmail.com>
2025-05-24 06:35:06 -0500

    Se crea interfaz ListaReproduccionRepository para gestionar operaciones de la lista de reproducción

---
+++ src/main/java/com/playlist/domain/port/ListaReproduccionRepository.java
@@ -0,0 +1,16 @@
+package com.playlist.domain.port;
+
+import com.playlist.domain.model.ListaReproduccion;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ListaReproduccionRepository {
+
+    ListaReproduccion save(ListaReproduccion lista);
+    Optional<ListaReproduccion> findByNombre(String nombre);
+    List<ListaReproduccion> findAll();
+    void deleteByNombre(String nombre);
+    boolean existsByNombre(String nombre);
+    
+}
